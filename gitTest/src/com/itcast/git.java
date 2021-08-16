e6bb3be63890f44aa535728a7b188e16685f68cc
liuxv <545550954@qq.com>
2021-08-16 20:50:42 +0800

    提交

--- gitTest/src/com/itcast/git.java
+++ gitTest/src/com/itcast/git.java
@@ -4,4 +4,8 @@ public class git {
     public static void main(String[] args) {
         System.out.println("hellogit");
     }
+    public void test(){
+        int i = 10;
+        i = i+1;
+    }
 }
